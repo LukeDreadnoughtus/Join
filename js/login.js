9d06f96d43a0b9b0ca64c2d0cbe645bd966e3b67
Johanna-Flock <johanna.j.f@web.de>
2025-10-21 19:34:52 +0200

    login function 2

--- js/login.js
+++ js/login.js
@@ -1,16 +1,34 @@
-async function logIn() {
+let path = "https://joinregistration-d9005-default-rtdb.europe-west1.firebasedatabase.app/"
+
+
+async function logIn(event) {
+    event.preventDefault();
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value
-    let resultUserName = await fetch ()
-    let resultPassword = await fetch ()
-    let currentUserName = await response.json()
-    let currentPassword = await response.json()
-if (username === currentUserName && password === currentPassword) {
-    window.location.href = "summary.html";
+    try {
+        const response = await fetch(path + ".json");
+        const userData = await response.json();
+        proofUserData(userData, username, password)
+
+     } catch (error) {
+        console.error("Fehler beim Login:", error);
+        alert("Ein Fehler ist aufgetreten. Bitte versuche es später erneut.");
     }
-    else {
-      alert("Dein Passwort und/oder dein eingegebener Benutzername stimmen nicht.");
+}
+
+function proofUserData(userData, username, password) {
+     for (let i = 1; i< Object.keys(userData).length; i++) {
+        let userKey = "user" + i;
+        let user = userData[userKey]
+
+        if (!user) continue;
+
+        if (username === user.name && password === user.password) {
+            window.location.href = "summary.html";
+            return
+        }
     }
+    alert("Dein Passwort und/oder dein eingegebener Benutzername stimmen nicht.");
 }
 
 
