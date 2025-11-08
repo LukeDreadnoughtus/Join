c3a566209357fabd8e8815042eed5f2c22ee9c20
Michael Doll <dollmichael53@gmail.com>
2025-11-08 18:34:35 +0100

    Implement task overlay functionality for adding new tasks

---
+++ js/board.js
@@ -0,0 +1,13 @@
+function openAddTaskOverlay() {
+    document.getElementById("taskCeateButton").addEventListener("click", () => {
+        const overlay = document.getElementById("overlay");
+        const frame = document.getElementById("add-task-frame");
+        frame.src = "add_task.html";
+        overlay.classList.remove("hidden");
+    });
+    document.getElementById("overlay").addEventListener("click", (e) => {
+        if (e.target.id === "overlay") e.target.classList.add("hidden");
+    });
+}
+
+openAddTaskOverlay();
