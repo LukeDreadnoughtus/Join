857cf6162872d2fddcc0cc3dd8ff4c6528ceba7d
Michael Doll <dollmichael53@gmail.com>
2025-11-19 13:25:31 +0100

    Add notification system for subtask actions and improve subtask management

--- js/addtask.js
+++ js/addtask.js
@@ -4,6 +4,24 @@ const lowBtn = document.getElementById('low');
 const mediumBtn = document.getElementById('medium');
 const urgentBtn = document.getElementById('urgent');
 const hiddenInput = document.getElementById('task-priority');
+const MAX_SUBTASKS = 3;
+const editIcon = `
+<svg width="19" height="19" viewBox="0 0 19 19" fill="none">
+  <path d="M2 16.25H3.4L12.025 7.625L10.625 6.225L2 14.85V16.25ZM16.3 6.175L12.05 1.975L13.45 0.575C13.8333 0.191667 14.3042 0 14.8625 0C15.4208 0 15.8917 0.191667 16.275 0.575L17.675 1.975C18.0583 2.35833 18.2583 2.82083 18.275 3.3625C18.2917 3.90417 18.1083 4.36667 17.725 4.75L16.3 6.175ZM14.85 7.65L4.25 18.25H0V14L10.6 3.4L14.85 7.65Z" fill="#2A3647"/>
+</svg>`;
+const deleteIcon = `
+<svg width="16" height="18" viewBox="0 0 16 18" fill="none">
+  <path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3Z" fill="#2A3647"/>
+</svg>`;
+const input = document.getElementById("subtask-tags");
+const addIcon = document.getElementById("subtask-add");
+const clearIcon = document.getElementById("subtask-clear");
+const list = document.getElementById("subtask-list");
+const divider = document.getElementById("dividerSubtasks");
+const delBtn = createButton(deleteIcon, () => {
+  item.remove();
+  updateAddUIState();
+});
 
 
 lowBtn.addEventListener('click', () => {
@@ -110,6 +128,7 @@ function clearForm() {
   const mediumBtn = document.getElementById('medium');
   if (mediumBtn) mediumBtn.classList.add('active');
   clearAssignedDropdown();
+  clearSubtasks();
 }
 
 
@@ -120,6 +139,13 @@ function clearAssignedDropdown() {
   if (window.selectedUsers) window.selectedUsers.clear();
   const display = container.querySelector(".select-display");
   updateDisplayText();
+}
+
+
+function clearSubtasks() {
+  document.getElementById("subtask-list").innerHTML = "";
+  clearDivider();
+  updateAddUIState();
 }
 
 
@@ -226,12 +252,6 @@ function updateDisplayText() {
 }
 
 
-const input = document.getElementById("subtask-tags");
-const addIcon = document.getElementById("subtask-add");
-const clearIcon = document.getElementById("subtask-clear");
-const list = document.getElementById("subtask-list");
-const divider = document.getElementById("dividerSubtasks");
-
 input.addEventListener("input", () => {
   const hasText = input.value.trim().length > 0;
   addIcon.classList.toggle("hidden", !hasText);
@@ -242,7 +262,6 @@ input.addEventListener("input", () => {
 addIcon.addEventListener("click", () => {
   const text = input.value.trim();
   if (!text) return;
-
   input.value = "";
   addIcon.classList.add("hidden");
   clearIcon.classList.add("hidden");
@@ -271,44 +290,151 @@ function hideIcons(addIcon, clearIcon, divider) {
   addIcon.classList.add("hidden"); clearIcon.classList.add("hidden"); divider.classList.add("hidden");
 }
 
-function addSubtask(text) {
-  if (!text) return;
-  const item = document.createElement("div"), left = document.createElement("div"), span = document.createElement("span"), dot = document.createElement("div"), buttons = document.createElement("div");
-  item.className = "subtask-item"; left.className = "subtask-left"; span.className = "subtask-text"; dot.className = "subtask-dot"; buttons.className = "subtask-buttons";
-  span.textContent = text; left.append(dot, span);
-  const editBtn = document.createElement("button_edit"), delBtn = document.createElement("button_delete"); editBtn.innerHTML = `<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M2 16.25H3.4L12.025 7.625L10.625 6.225L2 14.85V16.25ZM16.3 6.175L12.05 1.975L13.45 0.575C13.8333 0.191667 14.3042 0 14.8625 0C15.4208 0 15.8917 0.191667 16.275 0.575L17.675 1.975C18.0583 2.35833 18.2583 2.82083 18.275 3.3625C18.2917 3.90417 18.1083 4.36667 17.725 4.75L16.3 6.175ZM14.85 7.65L4.25 18.25H0V14L10.6 3.4L14.85 7.65Z" fill="#2A3647"/>
-</svg>`; delBtn.innerHTML = `<svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M3 18C2.45 18 1.97917 17.8042 1.5875 17.4125C1.19583 17.0208 1 16.55 1 16V3C0.716667 3 0.479167 2.90417 0.2875 2.7125C0.0958333 2.52083 0 2.28333 0 2C0 1.71667 0.0958333 1.47917 0.2875 1.2875C0.479167 1.09583 0.716667 1 1 1H5C5 0.716667 5.09583 0.479167 5.2875 0.2875C5.47917 0.0958333 5.71667 0 6 0H10C10.2833 0 10.5208 0.0958333 10.7125 0.2875C10.9042 0.479167 11 0.716667 11 1H15C15.2833 1 15.5208 1.09583 15.7125 1.2875C15.9042 1.47917 16 1.71667 16 2C16 2.28333 15.9042 2.52083 15.7125 2.7125C15.5208 2.90417 15.2833 3 15 3V16C15 16.55 14.8042 17.0208 14.4125 17.4125C14.0208 17.8042 13.55 18 13 18H3ZM3 3V16H13V3H3ZM5 13C5 13.2833 5.09583 13.5208 5.2875 13.7125C5.47917 13.9042 5.71667 14 6 14C6.28333 14 6.52083 13.9042 6.7125 13.7125C6.90417 13.5208 7 13.2833 7 13V6C7 5.71667 6.90417 5.47917 6.7125 5.2875C6.52083 5.09583 6.28333 5 6 5C5.71667 5 5.47917 5.09583 5.2875 5.2875C5.09583 5.47917 5 5.71667 5 6V13ZM9 13C9 13.2833 9.09583 13.5208 9.2875 13.7125C9.47917 13.9042 9.71667 14 10 14C10.2833 14 10.5208 13.9042 10.7125 13.7125C10.9042 13.5208 11 13.2833 11 13V6C11 5.71667 10.9042 5.47917 10.7125 5.2875C10.5208 5.09583 10.2833 5 10 5C9.71667 5 9.47917 5.09583 9.2875 5.2875C9.09583 5.47917 9 5.71667 9 6V13Z" fill="#2A3647"/>
-</svg>
-`;
-  editBtn.onclick = () => makeSubtaskEditable(item, span, buttons, editBtn, delBtn); delBtn.onclick = () => item.remove();
-  buttons.append(editBtn, delBtn); item.append(left, buttons); document.getElementById("subtask-list").appendChild(item);
+
+function createDot() {
+  const dot = document.createElement("div");
+  dot.className = "subtask-dot";
+  return dot;
 }
 
-function makeSubtaskEditable(item, span, buttons) {
-  const left = item.querySelector(".subtask-left");
-  const inputEdit = document.createElement("input");
-  inputEdit.type = "text"; inputEdit.value = span.textContent; inputEdit.className = "edit-input";
-  left.innerHTML = ""; left.appendChild(inputEdit);
-  buttons.style.display = "none";
-  const wrapper = document.createElement("div");
-  wrapper.className = "subtask-input-wrapper"; wrapper.appendChild(inputEdit);
-  const check = document.getElementById("subtask-add").cloneNode(true);
-  const cancel = document.getElementById("subtask-clear").cloneNode(true);
-  check.classList.remove("hidden"); cancel.classList.remove("hidden");
+function createSpan(text) {
+  const span = document.createElement("span");
+  span.className = "subtask-text";
+  span.textContent = text;
+  return span;
+}
 
-  check.onclick = () => {
-    span.textContent = inputEdit.value.trim() || span.textContent;
-    left.innerHTML = ""; left.appendChild(span);
-    buttons.style.display = "flex";
-  };
-  cancel.onclick = () => {
-    left.innerHTML = ""; left.appendChild(span);
-    buttons.style.display = "flex";
-  };
-  wrapper.append(check, cancel);
-  left.appendChild(wrapper);
+function createButton(html, onClick) {
+  const btn = document.createElement("button");
+  btn.innerHTML = html;
+  btn.onclick = onClick;
+  return btn;
+}
+
+function rebuildLeft(left, dot, contentElement) {
+  left.innerHTML = "";
+  left.append(dot, contentElement);
+}
+
+
+function addSubtask(text) {
+  if (!text.trim()) return;
+  const list = document.getElementById("subtask-list");
+  if (getSubtaskCount() >= MAX_SUBTASKS) {
+    showMessage("Du kannst maximal 3 Subtasks hinzufügen.", "error");
+    clearDivider();
+    updateAddUIState();
+    return;
+  }
+  const item = document.createElement("div");
+  item.className = "subtask-item";
+  const left = document.createElement("div");
+  left.className = "subtask-left";
+  const dot = createDot();
+  const span = createSpan(text);
+  left.append(dot, span);
+  const buttons = createButtons(item, span);
+  item.append(left, buttons);
+  list.appendChild(item);
+  clearDivider();
+  updateAddUIState();
+  document.getElementById("subtask-input").value = "";
+}
+
+function createButtons(item, span) {
+  const buttons = document.createElement("div");
+  buttons.className = "subtask-buttons";
+  const editBtn = createButton(editIcon, () => enterEditMode(item, span, buttons));
+  const delBtn = createButton(deleteIcon, () => item.remove());
+  buttons.append(editBtn, delBtn);
+  return buttons;
+}
+
+
+function enterEditMode(item, span, buttons) {
+  const left = item.querySelector(".subtask-left");
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = span.textContent;
+  input.className = "edit-input";
+  buttons.style.display = "none";
+  rebuildLeft(left, createDot(), input);
+  const check = cloneButton("subtask-add");
+  const cancel = cloneButton("subtask-clear");
+  check.onclick = () => saveEdit(left, span, input, buttons);
+  cancel.onclick = () => cancelEdit(left, span, buttons);
+  const wrapper = document.createElement("div");
+  wrapper.className = "subtask-input-wrapper";
+  wrapper.append(input, check, cancel);
+  left.append(wrapper);
+}
+
+
+function saveEdit(left, span, input, buttons) {
+  span.textContent = input.value.trim() || span.textContent;
+
+  rebuildLeft(left, createDot(), span);
+  buttons.style.display = "flex";
+}
+
+function cancelEdit(left, span, buttons) {
+  rebuildLeft(left, createDot(), span);
+  buttons.style.display = "flex";
+}
+
+
+function cloneButton(id) {
+  const btn = document.getElementById(id).cloneNode(true);
+  btn.classList.remove("hidden");
+  return btn;
+}
+
+
+function clearDivider() {
+  const divider = document.getElementById("dividerSubtasks");
+  if (!divider) return;
+  divider.classList.add("hidden");
+}
+
+
+function showMessage(message, type = "info") {
+  const container = document.getElementById("notification-container");
+  const note = document.createElement("div");
+  note.className = `notification ${type}`;
+  note.textContent = message;
+  container.appendChild(note);
+  setTimeout(() => {
+    note.style.animation = "fadeOut 0.3s forwards";
+  }, 2500);
+  setTimeout(() => {
+    note.remove();
+  }, 3000);
+}
+
+function getSubtaskCount() {
+  return document.querySelectorAll("#subtask-list .subtask-item").length;
+}
+
+function disableSubtaskInput() {
+  const input = document.getElementById("subtask-input");
+  const btn = document.getElementById("subtask-add-btn");
+  input.disabled = true;
+  btn.disabled = true;
+}
+
+function enableSubtaskInput() {
+  const input = document.getElementById("subtask-input");
+  const btn = document.getElementById("subtask-add-btn");
+  input.disabled = false;
+  btn.disabled = false;
+}
+
+function updateAddUIState() {
+  if (getSubtaskCount() >= MAX_SUBTASKS) {
+    disableSubtaskInput();
+  } else {
+    enableSubtaskInput();
+  }
 }
 
 
